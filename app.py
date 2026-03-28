7483d2f37ee3f45f0ac9f045de88d9abb3a63c3f
MarellaSahitya <sahityarao.marella@gmail.com>
2026-03-28 16:39:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -437,8 +437,8 @@ def admin_api_stats():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   # app.run(debug=True)
     import os
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
